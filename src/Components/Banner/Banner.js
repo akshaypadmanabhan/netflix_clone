81a6b9aa0ca6c6b31f420818dc15631834b255fd
Akshay Padmanabhan <akshayplearning@gmail.com>
2022-06-26 03:05:16 +0530

    Banner Completed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className="banner">
+        <div className="content">
+           <h1>Movie Name</h1>
+             <div className="banner-buttons">
+                <button className="button">Play</button>
+                <button className="button">More-info</button>
+
+           </div>
+           <h1 className="description">Part 4 (also referred to as the second part of season 2) Of Netflix's Money Heist consists of 8 episodes. It was released on April 3, 2020. Money Heist Part</h1>
+           </div>
+           <div className="fade-bottom"></div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Banner
