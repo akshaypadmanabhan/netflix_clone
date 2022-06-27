e94084d58994fa7adde81c95ed8fab862889cc7f
Akshay Padmanabhan <akshayplearning@gmail.com>
2022-06-27 22:24:00 +0530

    Updates

---
+++ precache-manifest.5dabe23d3706888928ab1447caf47ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bfc2d6f00d131e2817c",
+    "url": "/netflix_clone/static/css/main.ef836a11.chunk.css"
+  },
+  {
+    "revision": "6bfc2d6f00d131e2817c",
+    "url": "/netflix_clone/static/js/main.6bfc2d6f.chunk.js"
+  },
+  {
+    "revision": "e37e97a2a12b2d80808d",
+    "url": "/netflix_clone/static/js/1.e37e97a2.chunk.js"
+  },
+  {
+    "revision": "8b9452fe8a714fbfc8cf",
+    "url": "/netflix_clone/static/js/runtime~main.8b9452fe.js"
+  },
+  {
+    "revision": "8bef9c3d9d81fce166bf1292ae939205",
+    "url": "/netflix_clone/index.html"
+  }
+];
